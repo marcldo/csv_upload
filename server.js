f6b0b6da19012721b511cd4c44d62c281481e71d
gitMarcldo <marcldo@users.noreply.github.com>
2020-02-16 13:39:07 -0500

    server.js init and folder structure

---
+++ server.js
@@ -0,0 +1,26 @@
+
+// Dependencies
+// =============================================================
+const express = require("express");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express(static("app/public")));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
